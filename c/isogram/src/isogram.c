2800adef4867733ac997c4f6b2300cce3a1a2906
Alessandro Cipriani <alex.2.cipriani@gmail.com>
2021-04-19 22:58:07 +0200

    first isogram version

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,1 +1,24 @@
 #include "isogram.h"
+
+
+bool is_isogram(const char phrase[]){
+	if(phrase==NULL){
+		return false;
+	}
+
+	for(uint8_t i = 0; i < strlen(phrase); i++){
+		char selected = tolower(phrase[i]);
+	    uint8_t occurrence = 0;
+		if(selected != '-' && selected != ' '){
+			for(uint8_t j = 0; j < strlen(phrase); j++){
+				if(selected == tolower(phrase[j]))
+					occurrence++;
+				if(occurrence>1)
+					return false;
+			}
+	}
+	}
+
+
+	return true;
+}
--- c/isogram/src/isogram.h
+++ c/isogram/src/isogram.h
@@ -2,6 +2,9 @@
 #define ISOGRAM_H
 
 #include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
 
 bool is_isogram(const char phrase[]);
 
