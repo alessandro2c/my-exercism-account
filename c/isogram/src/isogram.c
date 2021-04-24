8d287a375fee82181362493572e8a764ac78297f
Alessandro Cipriani <>
2021-04-24 14:52:20 +0000

    only one for

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -6,27 +6,20 @@ bool is_isogram(const char phrase[]){
 		return false;
 	}
 
-	//for (size_t i = 0, len = strlen(phrase); i < len; i++)
-	// or
-	//for (size_t i = 0; phrase[i];  i++)
-	// or
-	//for (const char *cp = phrase; *cp; cp++)
+  char repetitions[26] = {0}; //char from 97 -112
 
+  for (const char *cp = phrase; *cp; cp++)
+  {
+    int selected = tolower((unsigned char)*cp);
+    if(selected != 45 && selected != 32 ){
+      int index = selected - 97;
 
-	for (size_t i = 0; phrase[i];  i++){
-		char selected = tolower((unsigned char)phrase[i]);
-	    uint8_t occurrence = 0;
-		if(selected != '-' && selected != ' '){
-			for (const char *cp = phrase; *cp; cp++)
-			{
-				if(selected == tolower((unsigned char)*cp))
-					occurrence++;
-				if(occurrence>1)
-					return false;
-			}
-	}
-	}
+      repetitions[index]++;
 
+      if(repetitions[index] > 1)
+        return false;
+    }
+  }
 
 	return true;
 }
